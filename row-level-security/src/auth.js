0519f8379ad8256f54392db33e409ad2ef80c1b0
Matt Hamrick <matthewjhamrick@gmail.com>
2019-06-20 12:31:05 +0000

    migrated to dscc-gen & Added simple README.md

---
+++ row-level-security/src/auth.js
@@ -0,0 +1,10 @@
+var cc = DataStudioApp.createCommunityConnector();
+
+// https://developers.google.com/datastudio/connector/reference#getauthtype
+function getAuthType() {
+  var AuthTypes = cc.AuthType;
+  return cc
+    .newAuthTypeResponse()
+    .setAuthType(AuthTypes.NONE)
+    .build();
+}
